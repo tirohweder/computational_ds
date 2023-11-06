ceee758df6d78da00c80fcb03f6f9dcc278c9e6e
Santi <storralbod@gmail.com>
2023-11-06 15:30:47 +0100

    filepaths added

---
+++ data/filepaths.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# file paths
+cnn_2014_file = '/home/OneDrive/OneDrive - Danmarks Tekniske Universitet/Year 1/Semester 1/Computational Tools for Data Science/Project/DATA/CNN_2014_2016/cnn_scraped_2014.csv'
+cnn_2015_file = '/home/OneDrive/OneDrive - Danmarks Tekniske Universitet/Year 1/Semester 1/Computational Tools for Data Science/Project/DATA/CNN_2014_2016/cnn_scraped_2015.csv'
+cnn_2016_file = '/home/OneDrive/OneDrive - Danmarks Tekniske Universitet/Year 1/Semester 1/Computational Tools for Data Science/Project/DATA/CNN_2014_2016/cnn_scraped_2016.csv'
+
