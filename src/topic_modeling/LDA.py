7ad89756b2d11a80dabd5e6e8eee467db6ca8239
DiegoJP02 <A01745889@itesm.mx>
2023-11-23 16:35:38 +0100

    Update LDA.py

--- src/topic_modeling/LDA.py
+++ src/topic_modeling/LDA.py
@@ -21,7 +21,10 @@ nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
 
-df=pd.read_csc("updated_dataframe_with_clusters_word2vec.csv")
+directory_output = os.path.join(os.path.dirname(__file__), '../..', 'data', 'output')
+directory_csv=os.path.join(directory_output,"updated_dataframe_with_clusters_word2vec.csv")
+df=pd.read_csv(directory_csv)
+
 
 def preprocess_text(df, text_column, custom_stopwords):
     clean_text_column = f"{text_column}_cleaned"
@@ -51,7 +54,7 @@ custom_stopwords = ["reuters", "fox", "cnn", "was", "has", "us", "year"]
 stop_words = set(stopwords.words('english'))
 
 # Call the function and update the DataFrame
-df_clean_text = preprocess_text(df[:5], "Text", custom_stopwords)
+df_clean_text = preprocess_text(df, "Text", custom_stopwords)
 
 # Save the DataFrame to a CSV file
 df_clean_text.to_csv('cleaned_dataframe.csv', index=False, encoding='utf-8')
@@ -62,7 +65,7 @@ dictionary = corpora.Dictionary(df_clean_text["Text_cleaned"])
 # Create a corpus from the documents
 corpus = [dictionary.doc2bow(doc) for doc in df_clean_text["Text_cleaned"]]
 
-lda_model_F = LdaModel(corpus, num_topics=2000, id2word=dictionary, passes=10)
+lda_model_F = LdaModel(corpus, num_topics=2, id2word=dictionary, passes=2)
 
 vis = pyLDAvis.gensim_models.prepare(lda_model_F, corpus, dictionary)
 
